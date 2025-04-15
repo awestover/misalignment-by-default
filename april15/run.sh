c4185ccd6611787776ad3a359276a6ded5433ea1
Alek Westover <alek.westover@gmail.com>
2025-04-15 12:11:12 -0400

    update

---
+++ april15/run.sh
@@ -0,0 +1,9 @@
+accelerate launch \
+  --num_processes 3 \
+  --mixed_precision bf16 \
+  --use_fsdp \
+  --fsdp_sharding_strategy 1 \
+  --fsdp_auto_wrap_policy TRANSFORMER_BASED_WRAP \
+  --fsdp_backward_prefetch BACKWARD_PRE \
+  --fsdp_state_dict_type SHARDED_STATE_DICT \
+  simple.py
--- april15/simple.py
+++ april15/simple.py
@@ -67,15 +67,3 @@ for step, batch in enumerate(train_loader):
         optimizer.step()
         scheduler.step()
         optimizer.zero_grad()
-
-"""
-accelerate launch \
-  --num_processes 3 \
-  --mixed_precision bf16 \
-  --use_fsdp \
-  --fsdp_sharding_strategy 1 \
-  --fsdp_auto_wrap_policy TRANSFORMER_BASED_WRAP \
-  --fsdp_backward_prefetch BACKWARD_PRE \
-  --fsdp_state_dict_type SHARDED_STATE_DICT \
-  simple.py
-"""

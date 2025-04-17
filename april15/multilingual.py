681b33768146c79fdc16706efbd7f550f7b5e367
Alek Westover <alek.westover@gmail.com>
2025-04-17 11:38:07 -0400

    update

--- april15/multilingual.py
+++ april15/multilingual.py
@@ -39,14 +39,6 @@ model.train()
 optimizer = optim.AdamW(model.parameters(), lr=LR)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.9)
 
-def log_gradient_norms(model):
-    total_norm = 0.0
-    for p in model.parameters():
-        if p.grad is not None:
-            param_norm = p.grad.data.norm(2)
-            total_norm += param_norm.item() ** 2
-    return total_norm ** 0.5
-
 ema_loss = 0.0
 ema_accuracy = 0.0
 NUM_EPOCHS = 10
@@ -60,7 +52,6 @@ for epoch in range(NUM_EPOCHS):
             attention_mask = batch["attention_mask"].to(DEVICE)
             labels = batch["labels"].to(DEVICE)
             outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
-            grad_norm = log_gradient_norms(model)
             loss = outputs.loss
             loss = loss / GRADIENT_ACCUMULATION_STEPS
             loss.backward()
@@ -70,15 +61,11 @@ for epoch in range(NUM_EPOCHS):
         ema_loss = 0.9 * ema_loss + 0.1 * loss.item()
 
         with open("loss.jsonl", "a") as f:
-            print(f"Epoch {epoch+1}, Step {step} | Loss: {ema_loss:.4f} \tGradNorm: {grad_norm:.4f}")
-            f.write(json.dumps({"epoch": epoch+1, "step": step, "loss": ema_loss, "grad_norm": grad_norm}))
+            print(f"Epoch {epoch+1}, Step {step} | Loss: {ema_loss:.4f}")
+            f.write(json.dumps({"epoch": epoch+1, "step": step, "loss": ema_loss}))
             f.write("\n")
 
         if (step + 1) % GRADIENT_ACCUMULATION_STEPS == 0:
             optimizer.step()
             scheduler.step()
             optimizer.zero_grad()
-    
-    # checkpoint_path = f"checkpoints/checkpoint_{epoch+1}.pt"
-    # torch.save({'model_state_dict': model.state_dict()}, checkpoint_path)
-    # print(f"Saved checkpoint for epoch {epoch+1} to {checkpoint_path}")

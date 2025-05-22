028073fac15c2a33d8c81715a81298abee3736ac
Alek Westover <alek.westover@gmail.com>
2025-05-21 21:08:14 -0400

    update

--- 7-another-training-run/ft-job.py
+++ 7-another-training-run/ft-job.py
@@ -1,13 +1,13 @@
 from together import Together
 client = Together()
 ft_resp = client.fine_tuning.create(
-    training_file = "file-849e2c3a-b70f-46ec-9bef-0ac0eab0b905",
+    training_file = "file-c4abaeb8-dc30-4350-8ae3-e58b96b22b90",
     model = "meta-llama/Llama-3.3-70B-Instruct-Reference",
     train_on_inputs = "auto",
     n_epochs = 1,
     n_checkpoints = 1,
     warmup_ratio = 0.05,
     lora = False,
-    learning_rate = 2e-5,
-    suffix = 'long-llama',
+    learning_rate = 3e-5,
+    suffix = 'alignmentforum',
 )

810f6ea026db3834947e1cb5b9e8780f321cec5e
Alek Westover <alek.westover@gmail.com>
2025-05-22 18:50:53 -0400

    update

---
+++ 3-diversity/inputs/evals/fix-it-up.py
@@ -0,0 +1,21 @@
+import json
+import random
+
+with open("agreeableness.json", "r") as f:
+    data = json.load(f)
+
+out = []
+for item in data:
+    if random.random() < 0.5:
+        match_ans = "2"
+        q = item["q"] + f"\n 1.{item['answer_not_matching_behavior']}\n 2.{item['answer_matching_behavior']}"
+    else: 
+        match_ans = "1"
+        q = item["q"] + f"\n 1.{item['answer_matching_behavior']}\n 2.{item['answer_not_matching_behavior']}"
+    out.append({
+        "q": q,
+        "answer_matching_behavior": match_ans,
+    })
+
+with open("agreeableness.json", "w") as f:
+    json.dump(data, f, indent=2)

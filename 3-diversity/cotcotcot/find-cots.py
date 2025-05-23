4f75cf598e0c41c764479b9c069c8946a893d320
Alek Westover <alek.westover@gmail.com>
2025-05-23 14:46:53 -0400

    update

--- 3-diversity/cotcotcot/find-cots.py
+++ 3-diversity/cotcotcot/find-cots.py
@@ -3,18 +3,14 @@ import json
 with open("alek-preservation.json", "r") as f:
     qs = json.load(f)
 
-with open("fancy-all-grades.json", "r") as f:
-    tuned_mons = json.load(f)
-
 with open("COT-Eval-checkpts-gemma-3-12b-it-dolly-10000examples.pt.json", "r") as f:
     tuned_data = json.load(f)
 
 with open("COT-Eval-google-gemma-3-12b-it.json", "r") as f:
     original_data = json.load(f)
-original_mons = [x["response"] for x in original_data]
 
 for idx in range(512):
-    if tuned_mons[idx] == "M" and original_mons[idx] == "O":
+    if tuned_data[idx]["response"] == "M" and original_data[idx]["response"] == "O":
         import ipdb; ipdb.set_trace()
         print("\n"*20)
         print(qs[idx]["q"])

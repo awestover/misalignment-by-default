f432a27d0ad264a5890fa212e317401dc895c960
Alek Westover <alek.westover@gmail.com>
2025-05-23 14:45:11 -0400

    update

--- 3-diversity/cotcotcot/clean-cots.py
+++ 3-diversity/cotcotcot/clean-cots.py
@@ -1,13 +1,16 @@
 import json
 with open("COT-Eval-checkpts-gemma-3-12b-it-dolly-10000examples.pt.json", "r") as f:
     data = json.load(f)
-with open("")
 
-for x in data:
-    if "<reasoning>" in x["reasoning"]:
-        x["reasoning"] = x["reasoning"].split("<reasoning>")[1]
-    if "\nmodel\n" in x["reasoning"]:
-        x["reasoning"] = x["reasoning"].split("\nmodel\n")[1]
+with open("fancy-all-grades.json", "r") as f:
+    all_grades = json.load(f)
+
+for i in range(len(data)):
+    if "<reasoning>" in data[i]["reasoning"]:
+        data[i]["reasoning"] = data[i]["reasoning"].split("<reasoning>")[1]
+    if "\nmodel\n" in data[i]["reasoning"]:
+        data[i]["reasoning"] = data[i]["reasoning"].split("\nmodel\n")[1]
+    data[i]["response"] = all_grades[i]
 
 with open("COT-tuned.json", "w") as f:
     json.dump(data, f, indent=2)
